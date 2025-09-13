883c9be94b67a86db51b9dec647f3d0d5db85a4d
Nischhelm <nischhelm@gmail.com>
2025-09-13 10:33:33 +0200

    fixed incontrol x optifine mixin
    returned too early, didnt apply DENY actions, making mobs spawn that shouldnt spawn

--- src/main/java/fermiummixins/mixin/incontrol/SpawnRule_OptifineMixin.java
+++ src/main/java/fermiummixins/mixin/incontrol/SpawnRule_OptifineMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import mcjty.incontrol.rules.SpawnRule;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
 
@@ -17,15 +17,15 @@ import java.util.UUID;
 public abstract class SpawnRule_OptifineMixin {
 
     @Inject(
-            method = "match(Lnet/minecraftforge/event/entity/living/LivingSpawnEvent$CheckSpawn;)Z",
+            method = "action(Lnet/minecraftforge/event/entity/living/LivingSpawnEvent$CheckSpawn;)V",
             at = @At("HEAD"),
             remap = false,
             cancellable = true
     )
-    private void fermiummixins_inControlSpawnRule_matchCheckSpawn(LivingSpawnEvent.CheckSpawn event, CallbackInfoReturnable<Boolean> cir) {
+    private void fermiummixins_inControlSpawnRule_matchCheckSpawn(LivingSpawnEvent.CheckSpawn event, CallbackInfo ci) {
         UUID uuid = event.getEntity().getUniqueID();
         if(InControlOptiFineHandler.savedUUIDS.contains(uuid)) {
-            cir.setReturnValue(false); //Don't apply actions on mobs that already have had those actions applied
+            ci.cancel(); //Don't apply actions on mobs that already have had those actions applied
             //Only needed when using OptiFine, as OptiFine caches constructed entities and reuses them for CheckSpawn until spawn the entity succeeds (risky fix by OptiFine but alright)
         }
         else InControlOptiFineHandler.savedUUIDS.add(uuid);
