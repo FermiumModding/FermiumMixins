2548bee9a2d23bb9fd052c1178b25e3b0d6dbc48
fonnymunkey <fonnymunkey@gmail.com>
2025-09-24 19:39:28 -0400

    1.0.8
    Fix lazy chunk spawning patch to account for pack spawning and slightly improve performance further

--- src/main/java/fermiummixins/FermiumMixins.java
+++ src/main/java/fermiummixins/FermiumMixins.java
@@ -13,7 +13,7 @@ import fermiummixins.proxy.CommonProxy;
 public class FermiumMixins {
     
     public static final String MODID = "fermiummixins";
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
     public static final String NAME = "FermiumMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
