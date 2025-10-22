221bf099060aba253f1e6445d13143632268a22c
Nischhelm <nischhelm@gmail.com>
2025-10-22 22:39:58 +0200

    fix dsurround chatting regex
    fonny had a fix but didnt apply it correctly. must have been a really old mixin
    also fixed a few other small issues with the regex, see comments in file

--- src/main/java/fermiummixins/mixin/dynamicsurroundings/EntityChatEffectWeightTableBuilder_CustomChatMixin.java
+++ src/main/java/fermiummixins/mixin/dynamicsurroundings/EntityChatEffectWeightTableBuilder_CustomChatMixin.java
@@ -1,13 +1,24 @@
 package fermiummixins.mixin.dynamicsurroundings;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.regex.Pattern;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(targets = "org.orecruncher.dsurround.client.handlers.effects.EntityChatEffect$WeightTableBuilder")
 public abstract class EntityChatEffectWeightTableBuilder_CustomChatMixin {
-    
-    @Shadow(remap = false)
-    private Pattern TYPE_PATTERN = Pattern.compile("chat\\.([a-zA-Z_.]*)\\.[0-9]*$");
+
+    @ModifyArg(
+            method = "<init>",
+            at = @At(value = "INVOKE", target = "Ljava/util/regex/Pattern;compile(Ljava/lang/String;)Ljava/util/regex/Pattern;", ordinal = 0),
+            remap = false
+    )
+    private String fermiumMixins_dynamicSurroundingsEntityChatEffect$WeightTableBuilder_init(String regex){
+        //original regex: chat\\.([a-zA-Z.]*)\\.[0-9]*$
+        //difference:
+        // - allow underscores in entity name
+        // - don't allow "chat..0", require at least one field in between the two outer periods
+        // - don't allow "chat......0", require at least one letter or underscore in between each set of periods
+        // - don't allow "chat.xyz.", require at least one digit at the end
+        return "chat\\.([a-zA-Z_]+\\.)+\\d+$";
+    }
 }
