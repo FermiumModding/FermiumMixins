651f0ef233648779887135b53e324637f461ef9f
fonnymunkey <fonnymunkey@gmail.com>
2026-02-18 21:20:20 -0500

    Bump version 1.0.12

--- src/main/java/fermiummixins/FermiumMixins.java
+++ src/main/java/fermiummixins/FermiumMixins.java
@@ -15,7 +15,7 @@ import fermiummixins.proxy.CommonProxy;
 public class FermiumMixins {
     
     public static final String MODID = "fermiummixins";
-    public static final String VERSION = "1.0.11";
+    public static final String VERSION = "1.0.12";
     public static final String NAME = "FermiumMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
