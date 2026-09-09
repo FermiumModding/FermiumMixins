294857dc2dd8920e739ce000f716715c060a79b7
nischhelm <nischhelm@gmail.com>
2026-09-09 10:22:54 +0200

    allow other mixins to modify golden osmosis

--- src/main/java/fermiummixins/mixin/reskillable/TraitGoldenOsmosis_BookWyrmMixin.java
+++ src/main/java/fermiummixins/mixin/reskillable/TraitGoldenOsmosis_BookWyrmMixin.java
@@ -1,21 +1,22 @@
 package fermiummixins.mixin.reskillable;
 
 import codersafterdark.reskillable.skill.magic.TraitGoldenOsmosis;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import fermiummixins.wrapper.DefiledLandsWrapper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TraitGoldenOsmosis.class)
 public abstract class TraitGoldenOsmosis_BookWyrmMixin {
 
-    @Redirect(
+    @WrapOperation(
             method = "tryRepair",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;getIsRepairable(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z")
     )
-    private boolean fermiummixins_reskillableTraitGoldenOsmosis_tryRepair(Item instance, ItemStack toRepair, ItemStack repair) {
-        return instance.getIsRepairable(toRepair, repair) || instance.getIsRepairable(toRepair, new ItemStack(DefiledLandsWrapper.getGoldenBookWyrmScale()));
+    private boolean fermiummixins_reskillableTraitGoldenOsmosis_tryRepair(Item instance, ItemStack toRepair, ItemStack repair, Operation<Boolean> original) {
+        return original.call(instance, toRepair, repair) || original.call(instance, toRepair, new ItemStack(DefiledLandsWrapper.getGoldenBookWyrmScale()));
     }
 }
